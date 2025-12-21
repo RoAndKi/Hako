45dac7f40f9d040f95229996417464976b375ee4
RoAndKi <waxhyze@gmail.com>
2025-12-21 18:35:01 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCApV_aY28T3Q0rb51y7202dvQByIwFx48",
-    authDomain: "hako-messenger.firebaseapp.com",
-    projectId: "hako-messenger",
-    storageBucket: "hako-messenger.firebasestorage.app",
-    messagingSenderId: "419229923192",
-    appId: "1:419229923192:web:715c8037275f3a1b90ba8d",
+  apiKey: "AIzaSyCApV_aY28T3Q0rb51y7202dvQByIwFx48",
+  authDomain: "hako-messenger.firebaseapp.com",
+  projectId: "hako-messenger",
+  storageBucket: "hako-messenger.firebasestorage.app",
+  messagingSenderId: "419229923192",
+  appId: "1:419229923192:web:715c8037275f3a1b90ba8d",
+  measurementId: "G-Q8G0G40JE9"
 };
 
 firebase.initializeApp(firebaseConfig);
