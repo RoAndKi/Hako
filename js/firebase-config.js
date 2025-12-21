008e42cf883f69a8aa52d0785ba3ede3fe6c1b37
RoAndKi <waxhyze@gmail.com>
2025-12-21 18:23:48 +0300

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCApV_aY28T3Q0rb51y7202dvQByIwFx48",
+  authDomain: "hako-messenger.firebaseapp.com",
+  projectId: "hako-messenger",
+  storageBucket: "hako-messenger.firebasestorage.app",
+  messagingSenderId: "419229923192",
+  appId: "1:419229923192:web:715c8037275f3a1b90ba8d",
+  measurementId: "G-Q8G0G40JE9"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
